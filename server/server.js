d171fadeca03f51978a61e1f0e9699726c51eb58
mmaniga <mmaniga@beapi.fr>
2017-04-18 13:15:50 +0200

    add post, get route

--- server/server.js
+++ server/server.js
@@ -1,25 +1,37 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-const {mongoose} = require('./db/mongoose');
-const {Todo} = require('./models/todo');
-const {User} = require('./models/user');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
 
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-	var todo = new Todo({
-		text: req.body.text
-	});
-	todo.save().then((doc) => {
-		res.send(doc);
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+    console.log('new toDo');
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos', (req, res) => {
+	Todo.find().then((todos) => {
+		res.send({todos});
 	}, (e) => {
 		res.status(400).send(e);
 	})
 })
 
 app.listen(3000, () => {
-	console.log('started on port 3000');
+  console.log('Started on port 3000');
 });
+
+module.exports = {app};
