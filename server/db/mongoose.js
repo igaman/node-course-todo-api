638358acce50b73fa18a02b0cc183187552e33d2
mmaniga <mmaniga@beapi.fr>
2017-04-18 18:17:34 +0200

    Setup to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://igaman:448788@ds163360.mlab.com:63360/todoapp' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 const {ObjectID} = require('mongodb');
 
-cosnt port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 var app = express();
 
