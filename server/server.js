fd19c129a1870a8e5972dead316ec155e8a05adb
mmaniga <mmaniga@beapi.fr>
2017-04-19 13:06:53 +0200

    Add PATCH /todos/id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 const port = process.env.PORT || 3000;
 
@@ -36,7 +37,7 @@ app.get('/todos/:id', (req, res) => {
 	}).catch((e) => {
 		res.status(400).send();
 	});
-}) 
+});
 
 app.get('/todos', (req, res) => {
 	Todo.find().then((todos) => {
@@ -44,7 +45,42 @@ app.get('/todos', (req, res) => {
 	}, (e) => {
 		res.status(400).send(e);
 	})
-})
+});
+
+app.delete('/todos/:id', (req,res) => {
+	var id = req.params.id;
+	if(!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if(!todo) return res.status(404).send();
+		res.send(todo);
+	}).catch((e) => {
+		res.status(400).send();
+	});
+});
+
+app.patch('/todos/:id',(req, res) => {
+	var id = req.params.id;
+	var body = _.pick(req.body, ['text','completed']);
+	if(!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+	if(_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+		if (!todo) return res.status(404).send();
+		res.send({todo});
+	}).catch((e) => { 
+		res.status(400).send();
+	})
+});
+
 
 app.listen(port, () => {
   console.log('Started on port ',port);
