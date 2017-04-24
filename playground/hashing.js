945c2e244f165e41a59b623ddc832b6a45cd75c1
mmaniga <mmaniga@beapi.fr>
2017-04-24 12:23:38 +0200

    hashing password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,13 +1,26 @@
 const {SHA256} =require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+var password = '123abc!';
+/*bcrypt.genSalt(10, (err, salt) => {
+	bcrypt.hash(password, salt, (err, hash) => {
+		console.log(hash);
+	});
+});*/
 
-var data = {
+var hashedPassword = '$2a$10$p5m/lF0AuIFMd21/wdOACuBOJ8RCrM7FLLovVLixJFCij5dLipSs.';
+
+bcrypt.compare(password, hashedPassword, (err,res) => {
+	console.log(res);
+});
+
+/*var data = {
 	id: 10
 };
 
 var token = jwt.sign(data,'123abc');
 var decoded = jwt.verify(token, '123abc');
-console.log(decoded);
+console.log(decoded);*/
 
 /*var msg = 'I am user number 3';
 var hash = SHA256(msg).toString();
