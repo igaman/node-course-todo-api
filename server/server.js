1f117201e2a195ebf9f3516104355b839106aee4
mmaniga <mmaniga@beapi.fr>
2017-04-26 18:58:17 +0200

    add DELETE users/me/token route

--- server/server.js
+++ server/server.js
@@ -101,7 +101,7 @@ app.post('/users', (req, res) => {
 });
 
 // POST /users/login
-app.post('/users/login/', (req, res) => {
+app.post('/users/login', (req, res) => {
   var body = _.pick(req.body, ['email','password']);
   User.findByCredentials(body.email, body.password).then((user) => {
   	return user.generateAuthToken().then((token) => {
@@ -115,6 +115,14 @@ app.post('/users/login/', (req, res) => {
 
 app.get('/users/me/', authenticate, (req, res) => {
 	res.send(req.user);
+});
+
+app.delete('/users/me/token', authenticate, (req,res) => {
+	req.user.removeToken(req.token).then(() => {
+		res.status(200).send();
+	}, () => {
+		res.status(400).send();
+	});
 });
 
 app.listen(port, () => {
